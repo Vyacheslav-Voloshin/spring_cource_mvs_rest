aec72db29cd63c1ae5ff872493b88e4f6ead866c
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2023-05-17 20:35:12 +0300

    create MyRestController and method showAllEmployee

---
+++ src/main/java/com/voloshyn/spring/rest/controller/MyRestController.java
@@ -0,0 +1,24 @@
+package com.voloshyn.spring.rest.controller;
+
+import com.voloshyn.spring.rest.entity.Employee;
+import com.voloshyn.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+    @Autowired
+    private EmployeeService employeeService;
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployee(){
+        List<Employee> allEmployee = employeeService.getAllEmployees();
+        return allEmployee;
+
+    }
+
+}
