377d75688bf4b502a7981f82f62f06a7c609a1d4
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2023-05-17 20:46:13 +0300

    create  method getEmployee

--- src/main/java/com/voloshyn/spring/rest/controller/MyRestController.java
+++ src/main/java/com/voloshyn/spring/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.voloshyn.spring.rest.entity.Employee;
 import com.voloshyn.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class MyRestController {
         List<Employee> allEmployee = employeeService.getAllEmployees();
         return allEmployee;
 
+    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
     }
 
 }
